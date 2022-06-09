23b6c4260760a99d6225a164c71ee0144660f2c6
Billy Rodriguez <billyrdz@gmail.com>
2022-06-09 13:22:38 -0500

    working api

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.querySelector("button").addEventListener("click", apiRequest);
+
+async function apiRequest() {
+  const calloutName = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://simple-rapper-api.herokuapp.com/api/${calloutName}`
+    );
+    const data = await response.json();
+
+    console.log(data);
+    document.querySelector("h2").innerText = data.name;
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ server.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 8000;
+
+app.use(cors());
+
+// prettier-ignore
+const letters = {
+  'a': {
+    "name": "Alfa",
+  }, 
+  'b': {
+    "name": "Bravo",
+  }, 
+  'c': {
+    "name": "Charlie",
+  }, 
+  'd': {
+    "name": "Delta",
+  }, 
+  'e': {
+    "name": "Echo",
+  }, 
+  'f': {
+    "name": "Foxtrot",
+  }, 
+  'g': {
+    "name": "Golf",
+  }, 
+  'h': {
+    "name": "Hotel",
+  }, 
+  'i': {
+    "name": "India",
+  }, 
+  'j': {
+    "name": "Juliett",
+  }, 
+  'k': {
+    "name": "Kilo",
+  }, 
+  'l': {
+    "name": "Lima",
+  }, 
+  'm': {
+    "name": "Mike",
+  }, 
+  'n': {
+    "name": "November",
+  }, 
+  'o': {
+    "name": "Oscar",
+  }, 
+  'p': {
+    "name": "Papa",
+  }, 
+  'q': {
+    "name": "Quebec",
+  }, 
+  'r': {
+    "name": "Romeo",
+  }, 
+  's': {
+    "name": "Sierra",
+  }, 
+  't': {
+    "name": "Tango",
+  }, 
+  'u': {
+    "name": "Uniform",
+  }, 
+  'v': {
+    "name": "Victor",
+  }, 
+  'w': {
+    "name": "Whiskey",
+  }, 
+  'x': {
+    "name": "X-ray",
+  }, 
+  'y': {
+    "name": "Yankee",
+  }, 
+  'z': {
+    "name": "Zulu",
+  }
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api", (req, res) => {
+  res.json(letters);
+});
+
+app.get("/api/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  let arr = name.split("").map((el) => `<h2>${letters[el].name}</h2>`);
+  console.log(arr);
+  res.send(arr.join("<br>"));
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`);
+});
